e1839d403a9e558af16902ed80268c216056e6a8
mattscilipoti <matt.scilipoti@ga.co>
2015-10-15 08:35:29 -0400

    walker: adjust right border for window size.

--- walker.js
+++ walker.js
@@ -3,14 +3,14 @@ var walker = document.getElementById('walker');
 
 // Configure motion params:
 var walkingLeft = true;
-var leftBorder = 400;
-var rightBorder = 700;
-var speed = 5;
+var leftBorder = 0;
+var rightBorder = window.innerWidth - walker.offsetWidth;
+var speed = 10;
 var xPos = rightBorder;
 
 // Create an update function that will trigger every frame:
 function update() {
-  // Walk 5 pixels per iteration:
+  // Move "speed" pixels per iteration:
   xPos += walkingLeft ? -speed : speed;
   walker.style.left = xPos + "px";
 
@@ -25,8 +25,13 @@ function update() {
 setInterval(update, 100);
 
 
-// Setup a click handler that manually toggles walker direction: 
+// Setup a click handler that manually toggles walker direction:
 walker.addEventListener('click', function() {
   walker.classList.toggle("flip");
   walkingLeft = !walkingLeft;
+});
+
+// Change right border when resized
+window.addEventListener("resize", function(){
+  rightBorder = window.innerWidth - walker.offsetWidth;
 });
