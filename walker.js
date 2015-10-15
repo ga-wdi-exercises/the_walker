da07482a7a255d95eea9c21a13c903967892c233
mattscilipoti <matt.scilipoti@ga.co>
2015-10-15 08:14:00 -0400

    solution: move from solution dir

--- walker.js
+++ walker.js
@@ -2,9 +2,31 @@
 var walker = document.getElementById('walker');
 
 // Configure motion params:
+var walkingLeft = true;
 var leftBorder = 400;
 var rightBorder = 700;
+var speed = 5;
+var xPos = rightBorder;
 
-// Have the stick figure start at the right border and start walking left
-// When he crosses the left border, have him turn around and start walking right (and vice versa)
-// Bonus: Make him turn around when you click on him
+// Create an update function that will trigger every frame:
+function update() {
+  // Walk 5 pixels per iteration:
+  xPos += walkingLeft ? -speed : speed;
+  walker.style.left = xPos + "px";
+
+  // Turn around when you've crossed a border:
+  if (xPos < leftBorder || xPos > rightBorder) {
+    walker.classList.toggle("flip");
+    walkingLeft = !walkingLeft;
+  }
+};
+
+// Establish an update interval (framerate):
+setInterval(update, 100);
+
+
+// Setup a click handler that manually toggles walker direction: 
+walker.addEventListener('click', function() {
+  walker.classList.toggle("flip");
+  walkingLeft = !walkingLeft;
+});
