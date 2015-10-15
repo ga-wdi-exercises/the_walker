23d68e331d39d311a6b89e92bb1c6c837e396095
mattscilipoti <matt.scilipoti@ga.co>
2015-10-15 09:03:21 -0400

    walks right, resets to left at right border

--- walker.js
+++ walker.js
@@ -2,9 +2,35 @@
 var walker = document.getElementById('walker');
 
 // Configure motion params:
+var walkingLeft = false;
 var leftBorder = 0;
 var rightBorder = window.innerWidth - walker.offsetWidth;
+var speed = 10;
+var xPos = leftBorder;
 
-// Have the stick figure start at the right border and start walking left
-// When he crosses the left border, have him turn around and start walking right (and vice versa)
-// Bonus: Make him turn around when you click on him
+// The update function is called once every "frame" (via setInterval)
+// Changing the walker's position each frame, will cause him to move across the screen
+function update() {
+  // ensure walker is facing to the right (reverse of "normal")
+  // CSS does this for us, we just assign the right class.
+  walker.classList.add("flip");
+
+  // Move "speed" pixels per iteration:
+  xPos += speed;
+  // Reset back to left side, when we run into right wall
+  if(xPos >= rightBorder){
+    xPos = leftBorder;
+  }
+  // reposition the walker
+  walker.style.left = xPos + "px";
+};
+
+// Establish an update interval (framerate)
+// This will call the "update" method every 100ms
+// Add code to "update" to change the walker's position
+setInterval(update, 100);
+
+// Change right border when resized
+window.addEventListener("resize", function(){
+  rightBorder = window.innerWidth - walker.offsetWidth;
+});
